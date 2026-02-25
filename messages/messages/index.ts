dc8e8db26b57ae3fa85c8f39eead760ec9dd09e9
LUCIANO AI <info.focusmode.ai@gmail.com>
2026-02-25 10:43:13 -0800

    Delete messages/messages directory

--- messages/messages/index.ts
+++
@@ -1,5 +0,0 @@
-import type { AbstractIntlMessages } from 'next-intl';
-
-export async function getMessages(locale: string) {
-  return (await import(`./${locale}.json`)).default as AbstractIntlMessages;
-}
